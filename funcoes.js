94406fb832f056b9b779d649a498ea44aa022217
Vinicius <hutlun2014@gmail.com>
2020-11-01 14:03:22 -0300

    Grid para a segunda parte do site;

--- funcoes.js
+++ funcoes.js
@@ -1,18 +1,19 @@
 function load() {
-    console.log("Hello");
-    document.getElementById("nav_principal").style.display = "none";
+   
+
 }
 
 var lastScrollTop = 0;
 $(window).scroll(function(event){
    
     var st = $(this).scrollTop();
-
     if(st == 0) {
-        $("#nav_principal").slideUp("fast");
-       
-    }else{
-        $("#nav_principal").slideDown("slow");
+        $("#nav_principal").slideUp();
       
+    }else{
+     
+        
+        $("#nav_principal").slideDown();
+
     }
 });
